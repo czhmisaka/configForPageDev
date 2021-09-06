51e21dcc763d3a5f8195ce0e70bd2c3c47db3ffc
陈之罕 <wb-czh931449@cainiao.com>
2021-09-06 19:16:27 +0800

    czh

--- Config/index.js
+++ Config/index.js
@@ -6,7 +6,7 @@ const primeNum = require("../lib/prime");
 const reader = require('../lib/read');
 const num = require('../lib/num');
 const output = require('../lib/outPut');
-const sort = require('./lib/sort')
+const sort = require('../lib/sort')
 
     ! function () {}()
 const Util = {
